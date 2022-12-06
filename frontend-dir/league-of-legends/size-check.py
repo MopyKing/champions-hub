31fa9464f65905ca54c9c7b2c4cb7c957abe6984
MopyKing <roye16e@gmail.com>
2022-12-06 22:25:58 +0200

    full page with pictuers and buttons

---
+++ frontend-dir/league-of-legends/size-check.py
@@ -0,0 +1,6 @@
+from PIL import Image
+
+im = Image.open("/home/shido/projects/photos-hub/frontend-dir/league-of-legends/teemo.jpeg")
+print(im.size)
+
+## 1080 : zed3.jpg, poppy.png, vi-jinx.png, master-yi.png
--- frontend-dir/pages/pictures.py
+++ frontend-dir/pages/pictures.py
@@ -2,25 +2,46 @@ import streamlit as st
 from PIL import Image
 import os.path
 
-st.title("Pictures")
+st.title("Pictures - Credits to wallpapercave.com")
+new_height = 720
 
 ####### PICTURE OF ZED ########
-image =Image.open("/app/frontend-dir/league-of-legends/zed.png")
-st.image(image, caption='Shockblade Zed')
-st.button("Download")
+image =Image.open("/app/frontend-dir/league-of-legends/zed3.jpg")
+# resizing the image to 720p
+new_width = int(new_height / image.height *image.width)
+image.resize((new_width, new_height))
+st.image(image, caption='Zed')
+st.button("Download", key="1")
 
-####### PICTURE OF THRESH ########
-image =Image.open("/app/frontend-dir/league-of-legends/thresh.jpg")
-st.image(image, caption='Thresh')
-st.button("Download")
+####### PICTURE OF POPPY ########
+image =Image.open("/app/frontend-dir/league-of-legends/poppy.png")
+# resizing the image to 720p
+new_width = int(new_height / image.height *image.width)
+image.resize((new_width, new_height))
+st.image(image, caption='Poppy')
+st.button("Download", key="2")
 
 
-####### PICTURE OF TEEMO ########
-image =Image.open("/app/frontend-dir/league-of-legends/teemo.jpeg")
-st.image(image, caption='Teemo')
-st.button("Download")
+####### PICTURE OF VI AND JINX ########
+image =Image.open("/app/frontend-dir/league-of-legends/vi-jinx.png")
+# resizing the image to 720p
+new_width = int(new_height / image.height *image.width)
+image.resize((new_width, new_height))
+st.image(image, caption='Jinx-Vi')
+st.button("Download", key="3")
 
-####### PICTURE OF LOL ########
-image =Image.open("/app/frontend-dir/league-of-legends/teemo.jpeg")
-st.image(image, caption='League Of Legends')
-st.button("Download")
+####### PICTURE OF MASTER YI ########
+image =Image.open("/app/frontend-dir/league-of-legends/master-yi.png")
+# resizing the image to 720p
+new_width = int(new_height / image.height *image.width)
+image.resize((new_width, new_height))
+st.image(image, caption='Master Yi')
+st.button("Download", key="4")
+
+####### PICTURE OF KARTHUS ########
+image =Image.open("/app/frontend-dir/league-of-legends/karthus.png")
+# resizing the image to 720p
+new_width = int(new_height / image.height *image.width)
+image.resize((new_width, new_height))
+st.image(image, caption='Karthus')
+st.button("Download", key="5")
